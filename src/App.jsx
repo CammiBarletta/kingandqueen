dda2cf3786596d6cbf731bac376611217b7364c9
Camila Barletta <camilabarletta28@gmail.com>
2026-03-17 16:02:52 -0300

    App.jsx y main.jsx configurados

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react';
-import Nav from './components/Nav';
-import Main from './components/Main'; 
+
 import './index.css'
 
 function App() {
-  const [vista, setVista] = useState ("Home");
+
 
   return (
     <>
-      <Nav setVista={setVista} />
-      <Main vista={vista} />
-  
+    
     </>
   );
 }
