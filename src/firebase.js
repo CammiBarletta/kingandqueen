35a2a0f04fd15d283b7b297e9fa6c7f56a87897d
Camila Barletta <camilabarletta28@gmail.com>
2026-05-13 10:47:46 -0300

    fix: mover config firebase a variables de entorno

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
- 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDqvpQK5WGmreqH96aM72QWV4PW_vX6odI",
-  authDomain: "kingandqueen-78d0a.firebaseapp.com",
-  projectId: "kingandqueen-78d0a",
-  storageBucket: "kingandqueen-78d0a.firebasestorage.app",
-  messagingSenderId: "105940305854",
-  appId: "1:105940305854:web:721b71d63a3115ff255bef"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
- 
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
